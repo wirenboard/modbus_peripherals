a346df3fa04e563bdaf92652609e65d08dbe4e55
Evgeny Boger <boger@contactless.ru>
2015-11-13 20:09:28 +0300

    check number of arguments

--- tools/read_fw.sh
+++ tools/read_fw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "USAGE: $0 <port> <slave_id>"
+if [[ $# != 2 ]]; then
+    echo "USAGE: $0 <port> <slave_id>"
+fi
 
 modbus_client -mrtu $1 -s2 -pnone -a$2 -t0x04 -r220 -c24 | tail -n1 | cut -d':' -f2 | cut -c 2- | xargs -d'\n'  ~/work/board/modbus_peripherals/tools/fw_ver_decoder.py 
