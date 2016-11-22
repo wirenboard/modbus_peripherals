21d0903cfd98c9ef4620c0f0d307d2aeb1ac3293
Pavel Poglazov <poglazoff@gmail.com>
2016-11-22 21:27:00 +0400

    Fix
    Script gave errors while flashing WB-MR14. Flashing and even verification went smoothly but then were errors.
    Exact reason wasn't found. Removing "reset" parameter from "program" routine seems to make it work. Fix changes nothing meaningful.

--- commissioning/stm32_program_and_lock.sh
+++ commissioning/stm32_program_and_lock.sh
@@ -6,6 +6,6 @@ if [ $# -ne 1 ]
     exit 1
 fi
 
-RET=`openocd -f /usr/share/openocd/scripts/interface/stlink-v2.cfg -f /usr/share/openocd/scripts/target/stm32f0x.cfg -c "program $1 verify reset 0x8000000" -c "halt" -c "stm32f1x lock 0" -c "exit"`
+RET=`openocd -f /usr/share/openocd/scripts/interface/stlink-v2.cfg -f /usr/share/openocd/scripts/target/stm32f0x.cfg -c "program $1 verify 0x8000000" -c "halt" -c "stm32f1x lock 0" -c "exit"`
 
 exit $RET
