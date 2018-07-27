578dfd1a5dfb0e1fb6260f6eb7cd6fd220918788
Evgeny Boger <boger@contactless.ru>
2018-07-27 21:16:39 +0300

    use reset line

--- commissioning/stm32_unlock.sh
+++ commissioning/stm32_unlock.sh
@@ -6,4 +6,4 @@ if [ $# -ne 0 ]
     exit 1
 fi
 
-openocd -d0 -f /usr/share/openocd/scripts/interface/stlink-v2.cfg -f /usr/share/openocd/scripts/target/stm32f0x.cfg -c "init" -c "reset halt" -c "stm32f1x unlock 0" -c "exit"
+openocd -d0 -f /usr/share/openocd/scripts/interface/stlink-v2.cfg -f /usr/share/openocd/scripts/target/stm32f0x.cfg -c "reset_config srst_only srst_nogate connect_assert_srst" -c "init" -c "reset halt" -c "stm32f1x unlock 0" -c "exit"
