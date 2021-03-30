86fe1a733a67c5fedba1aa25224cb3118092fa83
Nikita Kulikov <nikitoz236@gmail.com>
2021-03-30 12:52:34 +0700

    stm32 use global path to openocd scripts

--- commissioning/stm32_read_flash.sh
+++ commissioning/stm32_read_flash.sh
@@ -6,6 +6,6 @@ if [ $# -ne 1 ]
     exit 1
 fi
 
-RET=`openocd -f /usr/share/openocd/scripts/interface/stlink-v2.cfg -f /usr/share/openocd/scripts/target/stm32f0x.cfg -c "init" -c "reset halt" -c "flash banks" -c "dump_image $1 0x08000000 0xf000" -c "poll" -c "exit"`
+RET=`openocd -f interface/stlink-v2.cfg -f target/stm32f0x.cfg -c "reset_config srst_only srst_nogate connect_assert_srst" -c "init" -c "reset halt" -c "flash banks" -c "dump_image $1 0x08000000 0xf000" -c "poll" -c "exit"`
 
 exit $RET
