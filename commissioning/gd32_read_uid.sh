4d03690a6e9ec0591896c05f899ac6f85f6177c8
vdromanov <vdromanov@yahoo.com>
2022-05-11 15:44:10 +0000

    added gd32-uuid-read script (#3)
    * added gd32-uid-read script

---
+++ commissioning/gd32_read_uid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STLINK_CONFIG="interface/stlink-v2.cfg"
+TARGET_CONFIG="$(dirname "$0")/gd32e230.cfg"
+UID_REG="0x1ffff7ac"
+
+
+function read_uid () {
+  ret=$(openocd -f $STLINK_CONFIG -f $TARGET_CONFIG -c "reset_config srst_only srst_nogate connect_assert_srst" -c "init" -c "reset halt" -c "mdw $UID_REG" -c "halt" -c "exit" 2>&1 >/dev/null | grep "$UID_REG: " | sed "s/$UID_REG: //")
+  echo $ret
+}
+
+
+read_uid
